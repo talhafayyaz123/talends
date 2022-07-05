d8a28a7ec6a98a1dd0969f5a01f15cc6ed4ce024
Muhammad Talha <talha.fayyaz52@gmail.com>
2022-07-05 21:06:13 +0500

    agency service pages

---
+++ resources/views/front-end/pages/video_animation.blade.php
@@ -0,0 +1,70 @@
+@extends('front-end.master2')
+
+@section('content')
+
+    <section class="theme_bg_dark pb-0">
+        <div class="container">
+            <div class="row align-items-end">
+                <div class="col-md-8 pb-5">
+                    <h1 class="text-white">A MINUTE OF VIDEO IS WORTH 1.8 MILLION WORDS</h1>
+                    <p class="text-white opcty_5">Videos and animations convey a complicated message pleasantly and playfully, better than written content or static graphics. They are one of the best marketing tools that transform a familiar concept and bring it to life. They grab the audience's attention faster than any marketing collateral, increasing customer base, and sales.</p>
+                </div>
+                <div class="col-md-4 ml-auto d-md-block d-none">
+                    <img src="{{ asset('uploads/company/content-header-img.png')}}" class="img-fluid" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section style="background-color:#efefef ;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2>Videos and animations</h2>
+                    <p>Videos and animations make a brand more engaging and interactive. They are an incredible asset to share across all social media channels, distribute through emails, and host on business websites—they're that versatile. Videos and animations account for more than <b>75%</b> of all online traffic, keeping visitors engaged on landing pages longer and convincing them to buy a product or service. An agency offering customized video and animation services helps a business build better brand awareness using powerful scriptwriting, gripping storyboard, alluring voiceover, and creative illustrations. Companies often don't want to invest in an in-house team of motion graphics designers to do these jobs and resort to outsourcing the project.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="pb-0">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h3 class="font-weight-bold mt-0">Talends is a work marketplace</h3>
+                    <p>Talends is a work marketplace – a platform that helps businesses find a partner who works with their marketing team to conceptualize a start-to-finish video or animation that serves user intent.  Talends assists businesses to hire a freelancer or find an agency to make customized videos and animations to engage the audience while fulfilling the business needs and marketing goals.Videos and animations are a fantastic way to connect with the audience by demonstrating brand messaging in a familiar and memorable format. They make abstract concepts into consumable content, emphasizing goals, values, mission, differentiators, etc. Talends enables companies to find an agency that offers a host of custom videos and animation products that propel a brand to the forefront of industry conversations.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 text-center text-md-left">
+                    <h3 class="text-theme">Video Explainer</h3>
+                    <p>To make your audience retain information and understand your brand, hire a freelancer proficient in making instructive and direct explainer videos.</p>
+                </div>
+                <div class="col-md-4 text-center text-md-left">
+                    <h3 class="text-theme">Product Promotions</h3>
+                    <p>Hire a freelancer specializing in showcasing your services and products in the best possible light through an animation or product video.</p>
+                </div>
+                <div class="col-md-4 text-center text-md-left">
+                    <h3 class="text-theme">Whiteboard Animation</h3>
+                    <p>Hire an agency that has the expertise to combine the power of art and narration in whiteboard animation.</p>
+                </div>
+                <div class="col-md-4 text-center text-md-left">
+                    <h3 class="text-theme">3D And Hybrid Videos</h3>
+                    <p>Hire the best agency that can go beyond 2D vector images and flat illustrations. The motion graphics team of highly professional agencies registered on Talends is equipped with the latest 3D and hybrid video technologies, enabling your brand to tap into advanced levels of multimedia sophistication.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="py-0">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 theme_bg_dark text-center custom-banner">
+                    <h1 class="text-white" style="font-size: 30px;">Leverage Talends to connect with the best local and offshore Agencies offering Video and Animation Services and elevate your marketing.</h1>
+                    <a href="javascript:;" class="btn btn-theme-white rounded-pill px-4 mt-4">Get Connected</a>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
