b1f4ddd254e78a3fe1b977ab5748e8f348167c17
Muhammad Talha <talha.fayyaz52@gmail.com>
2022-08-17 00:59:31 +0500

    connect page dynamic

--- resources/views/front-end/pages/connect.blade.php
+++ resources/views/front-end/pages/connect.blade.php
@@ -19,10 +19,10 @@
     <section class="theme_bg_dark">
         <div class="container">
             <div class="row">
-                <div class="col-md-6 order-md-2 mb-4">
+               <!--  <div class="col-md-6 order-md-2 mb-4">
                     <img src="{{ asset('talends/assets/img/connect.webp')}}" class="img-fluid" alt="">
-                </div>
-                <div class="col-md-6 order-md-1 mb-4">
+                </div> -->
+                <div class="col-md-12 order-md-1 mb-4">
                     <h5 class="text-white opcty_5">CONTACT OUR SUPPORT TEAM</h5>
                     <h1 class="text-white">We’re here 24/7</h1>
                     <p class="text-white opcty_5">Reach out with your questions, concerns and challenges. Or just to say hi. We’ll be happy to chat and help.</p>
@@ -41,19 +41,19 @@
                     <h2 class="font-weight-bold"><i>HELLO</i></h2>
                 </div>
                 <div class="col-md-9">
-                    <h2 class="mb-5">We're here for you Say hi to theFuture DYNAMICS</h2>
+                    <h2 class="mb-5">We're here for you Say hi to Talends</h2>
                     <div class="row">
                         <div class="col-md-6 mb-5">
                             <h3 class="font-weight-bold">Become a Client</h3>
-                            <a href="mailto:project@thefuturedynamics.com" class="text-dark">project@thefuturedynamics.com</a>
+                            <a href="mailto:enquiry.talends@gmail.com" class="text-dark">project@talends.com</a>
                         </div>
                         <div class="col-md-6 mb-5">
                             <h3 class="font-weight-bold">Become Agency Partner</h3>
-                            <a href="mailto:partner@thefuturedynamics.com" class="text-dark">partner@thefuturedynamics.com</a>
+                            <a href="mailto:enquiry.talends@gmail.com" class="text-dark">partner@talends.com</a>
                         </div>
                         <div class="col-md-6 mb-5">
-                            <h3 class="font-weight-bold">Join theFuture DYNAMICS</h3>
-                            <a href="mailto:work@thefuturedynamics.com" class="text-dark">work@thefuturedynamics.com</a>
+                            <h3 class="font-weight-bold">Join Talends</h3>
+                            <a href="mailto:enquiry.talends@gmail.com" class="text-dark">work@talends.com</a>
                         </div>
                     </div>
                 </div>
@@ -62,10 +62,10 @@
         </div>
     </section>
     <section class="theme_bg_dark text-white">
-        <div class="container py-md-5">
+        <div class="container py-md-3">
             <div class="row my-md-5">
-                <div class="col-md-4">
-                    <div class="d-flex">
+                <div class="col-md-12">
+                    <div class="d-flex" style="justify-content:center;justify-items:center;">
                         <i class="bi-geo-alt text-theme fa-2x mr-3"></i>
                         <div>
                             <h4 class="mt-0 font-weight-bold">Dubai</h4>
@@ -75,7 +75,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4">
+              <!--   <div class="col-md-4">
                     <div class="d-flex">
                         <i class="bi-geo-alt text-theme fa-2x mr-3"></i>
                         <div>
@@ -92,12 +92,15 @@
                             <p><i class="bi-globe"></i> 100-104 PCSIR Community Building, LAHORE</p>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
     </section>
     <section style="background-color:#f5f5f5;">
-        <div class="container">
+      @php
+      $footer_social_content=App\Helper::getFooterSocialContent();
+    @endphp
+    <div class="container">
             <div class="row align-items-center">
                 <div class="col-md-6 mb-4">
                     <img src="{{ asset('talends/assets/img/connect-us.webp')}}" class="img-fluid" alt="">
@@ -107,7 +110,7 @@
                     <h1 class="font-weight-bold">Connect with us</h1>
                     <ul class="list-inline">
                         <li class="px-2"> <a href="{{ $footer_social_content['banner_description'] ?? '' }}" target="_blank"><i class="fa fa-facebook fa-2x"></i></a> </li>
-                        <li class="px-2"> <a href="{{ $footer_social_content['project_description'] ?? '' }}" target="_blank"><i class="fa fa-twitter fa-2x"></i></a> </li>
+                        <!-- <li class="px-2"> <a href="{{ $footer_social_content['project_description'] ?? '' }}" target="_blank"><i class="fa fa-twitter fa-2x"></i></a> </li> -->
                         <li class="px-2"> <a href="{{ $footer_social_content['features_text'] ?? '' }}" target="_blank"><i class="fa fa-linkedin fa-2x"></i></a> </li>
                         <li class="px-2"> <a href="{{ $footer_social_content['work_description'] ?? '' }}" target="_blank"><i class="fa fa-instagram fa-2x"></i></a> </li>
                         <li class="px-2"> <a href="{{ $footer_social_content['services_description'] ?? '' }}" target="_blank"><i class="fa fa-youtube fa-2x"></i></a> </li>
@@ -136,7 +139,13 @@
                 <div class="col-md-9 mb-3">
                     <div class="content-box">
                         <h2>YOU'RE IN GOOD HANDS</h2>
-                        <form id="adminContactForm" method="post" action="">
+                        @if ($errors->any())
+                                @foreach ($errors->all() as $error)
+                                    <div class="error">{{$error}}</div>
+                                @endforeach
+                            @endif
+                            <form id="adminContactForm" method="post" action="{{ route('storeAdminLead') }}">
+                            @csrf
                             <div class="row">
                                 <div class="form-group col-md-6 mb-4">
                                     <input type="text" name="full_name"  value="" class="form-control form-control-lg"  placeholder="Name" id="full_name">
@@ -150,53 +159,16 @@
                                 <div class="form-group col-md-6 mb-4">
                                     <input type="text"  name='phone_number' class="form-control form-control-lg" placeholder="Phone Number" id="phone_number">
                                 </div>
-                                <div class="col-md-4">
-                                    <div class="custom-control custom-checkbox mb-3">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                        <label class="custom-control-label" for="customCheck1">UI/UX</label>
-                                    </div>
-                                    <div class="custom-control custom-checkbox mb-3">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                        <label class="custom-control-label" for="customCheck1">Web Development</label>
-                                    </div>
-                                    <div class="custom-control custom-checkbox mb-3">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                        <label class="custom-control-label" for="customCheck1">Mobile App Development</label>
-                                    </div>
-                                    <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                        <label class="custom-control-label" for="customCheck1">Enterprise Solutions</label>
-                                    </div>
+                                <div class="form-group col-md-12 mb-4">
+                                    <textarea rows="4" name="detail" id='detail' class="form-control form-control-lg">Describe your project</textarea>
                                 </div>
-                                <div class="col-md-4">
-                                    <div class="custom-control custom-checkbox mb-3">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                        <label class="custom-control-label" for="customCheck1">SEO</label>
-                                    </div>
-                                    <div class="custom-control custom-checkbox mb-3">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                        <label class="custom-control-label" for="customCheck1">Digital Content</label>
-                                    </div>
-                                    <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                        <label class="custom-control-label" for="customCheck1">Performance Marketing</label>
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="custom-control custom-checkbox mb-3">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                        <label class="custom-control-label" for="customCheck1">Business consulting</label>
-                                    </div>
-                                    <div class="custom-control custom-checkbox mb-3">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                        <label class="custom-control-label" for="customCheck1">Training with virtual reality</label>
-                                    </div>
-                                    <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                        <label class="custom-control-label" for="customCheck1">Ecommerce</label>
-                                    </div>
-                                </div>
-                                <div class="col-md-12 mt-4">
+                                <div class="form-group col-md-12 mb-4">
+                                {!! htmlFormSnippet() !!}
+                                        <span class="help-block" style="display: none;">
+                                            <strong class="error"></strong>
+                                        </span>
+                            </div>
+                                <div class="col-md-12">
                                     <button type="submit" class="btn btn-theme px-5 rounded-pill">Let us help you</button>
                                 </div>
                             </div>
@@ -208,6 +180,45 @@
     </section>
 @endsection
 @push('scripts')
+<script>
+    $(document).ready(function () {
+        $('#adminContactForm').validate({ 
+            rules: {
+                full_name: {
+                    required: true
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                company_name: {
+                    required: true,
+                    
+                },  phone_number: {
+                    required: true,
+                    
+                },
+
+                detail: {
+                    required: true,
+        
+                    
+                }
+            },
+            errorElement: 'span',
+            errorPlacement: function (error, element) {
+                error.addClass('invalid-feedback');
+                element.closest('.form-group').append(error);
+            },
+            highlight: function (element, errorClass, validClass) {
+                $(element).addClass('is-invalid');
+            },
+            unhighlight: function (element, errorClass, validClass) {
+                $(element).removeClass('is-invalid');
+            }
+        });
+    });
+</script> 
 <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
 <script>
         $('#connect-slides').owlCarousel({
