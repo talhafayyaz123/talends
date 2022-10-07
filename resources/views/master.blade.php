0cb153ead4d86373d2973b82ccea23128f534af8
Muhammad Talha <talha.fayyaz52@gmail.com>
2022-10-07 20:30:41 +0500

    tawk.to chat boot

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -138,7 +138,21 @@
     <script src="{{ asset('js/jquery-ui-min.js') }}"></script>
 	<script src="{{ asset('talends/assets/js/main.js') }}"></script>
 
-	 <script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/22223481.js"></script>
+	 <!-- <script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/22223481.js"></script> -->
+	 		<!--Start of Tawk.to Script-->
+			 <script type="text/javascript">
+		var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+		(function(){
+		var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+		s1.async=true;
+		s1.src='https://embed.tawk.to/6340099d54f06e12d898e9cc/1gep2b3ro';
+		s1.charset='UTF-8';
+		s1.setAttribute('crossorigin','*');
+		s0.parentNode.insertBefore(s1,s0);
+		})();
+		</script>
+		<!--End of Tawk.to Script-->
+		
     @stack('scripts')
     <script>		
         jQuery(window).load(function () {
